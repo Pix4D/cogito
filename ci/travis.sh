428fd562e0b03e4b8dada87fb4da8fcd640da618
Marco Molteni <marco.molteni@pix4d.com>
2019-11-01 16:39:54 +0100

    Build and release on Travis
    See CONTRIBUTING for how to issue a release
    (git tag -> test -> build image -> tag image -> publish)
    
    Each time a commit is built on a branch, an image with the branch name
    as tag is pushed.
    
    If a tag of the form v1.2.3 is found, an image with tags 1.2.3 and latest
    is pushed

---
+++ ci/travis.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+# Colors:
+# https://linuxhint.com/tput-printf-and-shell-expansions-how-to-create-awesome-outputs-with-bash-scripts/
+# https://unix.stackexchange.com/questions/269077/tput-setaf-color-table-how-to-determine-color-codes
+
+fgCyan=$(tput setaf 6)
+fgRed=$(tput setaf 1)
+fgGreen=$(tput setaf 2)
+txReset=$(tput sgr0)
+
+run() {
+    echo
+    echo "${fgCyan}RUN: $* ${txReset}"
+    if ! "$@" ; then
+        echo "${fgRed}FAIL: $* ${txReset}"
+        exit 1
+    fi
+    echo "${fgGreen}SUCCESS: $* ${txReset}"
+}
+
+run task test
+run task docker-build
+run task docker-smoke
+run task docker-push
