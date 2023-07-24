2c4c867e3e9277577e3146a49900dfeb02f1972d
Marco Molteni <marco.molteni@pix4d.com>
2023-07-24 15:05:16 +0200

    github: bring checkForRetry under direct test
    Function checkForRetry is already well-tested indirectly in the public
    API tests in commitstatus_test, but we want to prepare to test
    modifications to checkForRetry signature, so a direct test is more
    suited.

---
+++ github/commitstatus_private_test.go
@@ -0,0 +1,76 @@
+package github
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	"gotest.tools/v3/assert"
+)
+
+func TestCheckForRetrySuccess(t *testing.T) {
+	type testCase struct {
+		name         string
+		res          httpResponse
+		waitTime     time.Duration
+		maxSleepTime time.Duration
+		jitter       time.Duration
+		wantSleep    time.Duration
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		sleep, err := checkForRetry(tc.res, tc.waitTime, tc.maxSleepTime, tc.jitter)
+
+		assert.NilError(t, err)
+		assert.Equal(t, sleep, tc.wantSleep)
+	}
+
+	testCases := []testCase{
+		{
+			name:      "status OK: sleep==0",
+			res:       httpResponse{statusCode: http.StatusOK},
+			wantSleep: 0 * time.Second,
+		},
+		{
+			name:      "non retryable status code: sleep==0",
+			res:       httpResponse{statusCode: http.StatusTeapot},
+			wantSleep: 0 * time.Second,
+		},
+		{
+			name:      "retryable status code: sleep==waitTime",
+			res:       httpResponse{statusCode: http.StatusInternalServerError},
+			waitTime:  42 * time.Second,
+			wantSleep: 42 * time.Second,
+		},
+		{
+			name: "rate limited, would sleep too long: sleep==0",
+			res: httpResponse{
+				statusCode: http.StatusForbidden,
+				date:       time.Date(2001, time.April, 30, 13, 0, 0, 0, time.UTC),
+				// 1h after
+				rateLimitReset: time.Date(2001, time.April, 30, 14, 0, 0, 0, time.UTC),
+			},
+			maxSleepTime: 10 * time.Second,
+			wantSleep:    0 * time.Second,
+		},
+		{
+			name: "rate limited, would sleep a bit, adding also the jitter",
+			res: httpResponse{
+				statusCode: http.StatusForbidden,
+				date:       time.Date(2001, time.April, 30, 13, 0, 0, 0, time.UTC),
+				// 1h after
+				rateLimitReset: time.Date(2001, time.April, 30, 14, 0, 0, 0, time.UTC),
+			},
+			maxSleepTime: 2 * time.Hour,
+			jitter:       8 * time.Second,
+			wantSleep:    1*time.Hour + 8*time.Second,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) { run(t, tc) })
+	}
+}
+
+// BUG: sleeptime + jitter might cause a failure; test sleepTime > maxSleepTime should
+// be done before?
