558f12d778497501ba0122ea58436672971dd71c
Nimrod Wandera <wanderanimrod@gmail.com>
2023-10-13 11:48:17 -0500

    undo renaming of buildinfo -> buildInfo. The string is used to many other places.
    It is a non-trivial change that has nothing to do with GHE and one that requires its own PR.

--- cogito/buildinfo.go
+++ cogito/buildinfo.go
@@ -1,11 +1,11 @@
 package cogito
 
 // Baked in at build time with the linker. See the Taskfile and the Dockerfile.
-var buildInfo = "unknown"
+var buildinfo = "unknown"
 
 // BuildInfo returns human-readable build information (tag, git commit, date, ...).
 // This is useful to understand in the Concourse UI and logs which resource it is, since log
 // output in Concourse doesn't mention the name of the resource (or task) generating it.
 func BuildInfo() string {
-	return "This is the Cogito GitHub status resource. " + buildInfo
+	return "This is the Cogito GitHub status resource. " + buildinfo
 }
